62fba4c3ecb120e855a37e0e99253809479d3a25
yass741 <94975215+yass741@users.noreply.github.com>
2023-04-23 15:50:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
    plt.close()
-   mpl.rcParams['toolbar'] = 'None'
-   window = fenetre.lafenetre()
+   mpl.rcParams['toolbar'] = 'None'   # just to make sure that the plots are being well initialized
+   window = fenetre.lafenetre()   #window's initialization
    
 
-   window.chargerlafenetre()
+   window.chargerlafenetre()   #window's refreshing
