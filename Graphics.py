da1f8d7b9e31ad3590988a0e36e801d7e7b7d3a8
yass741 <94975215+yass741@users.noreply.github.com>
2023-04-23 15:49:53 +0200

    Update Graphics.py

--- Graphics.py
+++ Graphics.py
@@ -1,29 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from netCDF4 import Dataset
-from netCDF4 import Dataset
-import numpy as np
 from math import *
+from netCDF4 import Dataset
 import numpy.ma as ma
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+# from geopy import distance
 
-class GraphePrecip:
-    def __init__(self, year):
-        self.dataset = Dataset('./DataRepertory/precip.{}.nc'.format(year), 'r')
+# différentes classes correspondants aux différents types de graphes utilisés pour le frame local
+
+class Graph:
+    def __init__(self, year, type_donnees):
+        self.dataset = Dataset(f'./DataRepertory/{type_donnees}.{year}.nc', 'r')
         self.lats = self.dataset.variables['lat'][:]
         self.lons = self.dataset.variables['lon'][:]
         self.times = self.dataset.variables['time'][:]
-        self.vals = self.dataset.variables['precip'][:]
-        self.times = self.times - 964248
-        self.times = self.times / 24
-        self.times = self.times - (int(year) - 2010)*350 # des calculs afin d'afficher sur l'axe des x les jours
-                                                          #de manière visible et compréhensible
-        self.times = self.times - 100
-
-        
-        
+        self.vals = self.dataset.variables[type_donnees][:]
+        if(len(self.vals == 366)):
+            self.times = np.arange(0, len(self.vals), 1)
+        else:
+            self.times = np.arange(0, len(self.vals), 1)
         self.valeurs = None
-
-
 
     def find_closest_zone_values(self, lat, lon, vals):
            
@@ -34,7 +32,8 @@ class GraphePrecip:
             distances[:] = np.nan
 
            
-            for i in range(len(lats)):
+            for i in range(len(lats)):  #this was a complicated calculation and python is pretty much slow, but, with geopy library a specific method to
+                                            #calculate distances is available, but I didn't use it as I didn't know whether we have the right to use it
                 row_distances = []
                 for j in range(len(lons)):
                     a = pow(sin((float(lat)-float(lats[i]))/2),2) + cos(float(lat))*cos(float(lats[i]))*pow(sin(float(lon) - float(lons[j])/2),2)
@@ -42,11 +41,14 @@ class GraphePrecip:
                     R = 6373
                     row_distances.append(R*c)
 
+                # dist = distance.distance(point1, point2).km
+                # distances[i, j] = dist
                 
                 distances[i,:] = row_distances
 
            
-            min_index = np.unravel_index(np.nanargmin(distances), distances.shape)
+            min_index = np.unravel_index(np.nanargmin(distances), distances.shape)               #we append the different distances between the unmasked zones and the one in the input
+                                                                                                    # we then get the indexes of the unmasked zone that got the lowest distance
             print(vals.shape)
 
             
@@ -55,72 +57,72 @@ class GraphePrecip:
                 min_index = np.unravel_index(np.nanargmin(distances), distances.shape)
 
             return vals[:, min_index[0], min_index[1]]
+        
+
+class GraphePrecip(Graph):
+    def __init__(self, year):
+        super().__init__(year, 'precip')
+        
+    def DisplayGraph(self, year, lat, lon, frame, fig, gs):                 #on the contrary of the Maps, the matplotlib graphics were easier to display with 
+        plt.close()
+        plt.cla()
+        plt.clf()
+        plt.delaxes()
+        daily_values = []
+        self.valeurs = self.find_closest_zone_values(lat, lon, self.vals)
+        for day_values in self.vals:
+            daily_values.append(day_values.mean())
+
+        plot = fig.add_subplot(gs[1,0])
+        plot.set_xlabel("jours de l'année")
+        plot.set_ylabel("précipitations  ")
+        plot.plot(self.times, daily_values)       
+        canvas = FigureCanvasTkAgg(fig, master=frame) 
+
+
+        canvas.draw()
+        canvas.get_tk_widget().place(x =0, y=50)
+
+         
+
+
+
+
+class GrapheT(Graph):
+    def __init__(self, year):
+        super().__init__(year, 'tmin')
+        self.vals_tmax = None
+        self.tmin_daily_values = []
+        self.tmax_daily_values = []
+        
+    def DisplayGraph(self, year, lat, lon, frame, fig, gs):
+        plt.close()
+        plt.cla()
+        plt.clf()
+        plt.delaxes()
+        dataset = Dataset(f'./DataRepertory/tmax.{year}.nc', 'r')
+        self.valeurs = self.find_closest_zone_values(lat, lon, self.vals)
+        self.vals_tmax = self.find_closest_zone_values(lat, lon, dataset.variables['tmax'][:])
+        for day_values in self.valeurs:
+            self.tmin_daily_values.append(day_values)
+        for day_values in self.vals_tmax:
+            self.tmax_daily_values.append(day_values)
+        
+        ax1 = fig.add_subplot(gs[0,0])
+        ax1.set_xlabel("jours de l'année")
+        ax1.set_ylabel("températures minimales")
+        ax1.plot(self.times, self.tmin_daily_values)
+        
+        ax2 = ax1.twinx()
+        ax2.set_ylabel("températures maximales")
+        ax2.plot(self.times, self.tmax_daily_values, 'r')
+        ax1.legend(['températures minimales'], loc='upper left')
+        ax2.legend(['températures maximales'], loc='upper right')
+
+        canvas = FigureCanvasTkAgg(fig, master=frame) 
+        canvas.draw()
+        canvas.get_tk_widget().place(x=0, y=50)
+         
 
 
         
-    def DisplayGraph(self, year,lat, lon) :
-            plt.close()
-            daily_values = []
-            self.valeurs = self.find_closest_zone_values(lat, lon, self.dataset.variables['precip'][:])
-            for day_values in self.vals:
-                daily_values.append(day_values.mean())
-
-            plt.plot(self.times, daily_values)
-            plt.xlabel("Jours de l'année")
-            plt.ylabel("Précipitations (en mm)")
-            plt.title("Evolution annuelle des Précipiutations dans la zone sélectionnée {}".format(year))
-            plt.show()  
-
-
-class GrapheTmin:
-    def __init__(self, year, lon, lat):
-        self.dataset = Dataset('./DataRepertory/tmin.{}.nc'.format(year), 'r')
-        self.lats = self.dataset.variables['lat'][:]
-        self.lons = self.dataset.variables['lon'][:]
-        self.times = self.dataset.variables['time'][:]
-        self.vals = self.dataset.variables['tmin'][:]
-        self.times = self.times - 964248
-        self.times = self.times / 24
-        self.times = self.times - (int(year) - 2010)*350
-        self.times = self.times - 50
-        self.lat_id = np.abs(self.lats - float(lat)).argmin() 
-       
-        self.lon_id = np.abs(self.lons - float(lon)).argmin()
-        
-        self.valeurs = self.vals[:,self.lat_id, self.lon_id]
-        
-    def DisplayGraph(self, year) :
-            plt.close()
-            plt.plot(self.times, self.valeurs)
-            plt.xlabel("Jours de l'année")
-            plt.ylabel("Températures minimales (°C)")
-            plt.title("Evolution annuelle des températures minimales dans la zone sélectionnée {}".format(year))
-            plt.show()     
-            self.times = self.dataset.variables['time'][:]   
-
-class GrapheTmax:
-    def __init__(self, year, lon, lat):
-        self.dataset = Dataset('./DataRepertory/tmax.{}.nc'.format(year), 'r')
-        self.lats = self.dataset.variables['lat'][:]
-        self.lons = self.dataset.variables['lon'][:]
-        self.times = self.dataset.variables['time'][:]
-        self.vals = self.dataset.variables['tmax'][:]
-        self.times = self.times - 964248
-        self.times = self.times / 24
-        self.times = self.times - (int(year) - 2010)*350
-        self.times = self.times - 50
-
-
-        self.lat_proche_id = np.abs(self.lats - float(lat)).argmin() 
-       
-        self.lon_proche_id = np.abs(self.lons - float(lon)).argmin()
-        
-        self.valeurs = self.vals[:,self.lat_proche_id, self.lon_proche_id]
-        
-    def DisplayGraph(self, year) :
-            plt.close()
-            plt.plot(self.times, self.valeurs)
-            plt.xlabel("Jours de l'année")
-            plt.ylabel("Températures maximales (°C)")
-            plt.title("Evolution annuelle des températures maximales dans la zone sélectionnée {}".format(year))
-            plt.show()  
